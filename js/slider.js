eb6049248848ca00889f1249bcaba73b005d5e54
mabbieg <mgabri02@mail.uoguelph.ca>
2019-08-09 14:22:36 -0400

    front page slider & about page updates for fall 2019

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+var timer = setInterval(plusSlides, 3000);
+
+// Next/previous controls
+function plusSlides(n) {
+  if(n === undefined) {
+    n = 1;
+  }
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}
